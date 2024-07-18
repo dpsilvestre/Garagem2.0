9ee8079548e3b2d8643e4edef5723f26a421cde4
dpsilvestre <dp2132332@gmail.com>
2024-07-18 16:17:16 -0300

    Criar serializers modelo, veiculo e adicionar ao init

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -3,3 +3,5 @@ from .acessorio import AcessorioSerializer
 from .categoria import CategoriaSerializer
 from .cor import CorSerializer
 from .marca import MarcaSerializer
+from .modelo import ModeloSerializer
+from .veiculo import VeiculoSerializer
