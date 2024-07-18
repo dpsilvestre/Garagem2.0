eda86305390aca99c51584e14b5238a1bcf0e94f
dpsilvestre <dp2132332@gmail.com>
2024-07-18 17:25:08 -0300

    configurar passage

--- core/models/marca.py
+++ core/models/marca.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Marca(models.Model):
     nome = models.CharField(max_length=100)
-    site = models.URLField(max_length=200, blank=True, null=True)
-
+    nacionalidade = models.CharField(max_length=100)
     def __str__(self):
         return self.nome
